eb269bcd450ababf7848fa83294eccfd8a77208a
Massire <diabymassire93@gmail.com>
2026-02-26 11:28:23 +0000

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,12 @@
-gcc -Wall -Werror -Wextra -pedantic -std=gnu89 4-puts.c
-./a.out
+#include <stdio.h>
+
 /**
-*main - Entry point
-*
-*Return: Always 0 (Sucess)
-*/
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
+}
