4bcb3885d95ac80fab81ccc94d416db1a52e791f
Massire <diabymassire93@gmail.com>
2026-02-26 11:24:26 +0000

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,8 @@
+#!/bin/bash
+gcc -Wall -Werror -Wextra -pedantic -std=gnu89 4-puts.c
+./a.out
 /**
 *main - Entry point
 *
 *Return: Always 0 (Sucess)
 */
-gcc -Wall -Werror -Wextra -pedantic -std=gnu89 4-puts.c
-./a.out
