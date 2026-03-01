c237a473b9dfb1bace5c789d58bca8ca7f9a7baa
Massire <diabymassire93@gmail.com>
2026-03-01 21:44:58 +0000

    correction

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -24,11 +24,11 @@ printf(" and is greater than 5\n");
 }
 else if (last_digit == 0)
 {
-printf("and is zero\n");
+printf(" and is zero\n");
 }
 else
 {
-printf("and is less than 6 and not 0\n");
+printf(" and is less than 6 and not 0\n");
 }
 return (0);
 }
