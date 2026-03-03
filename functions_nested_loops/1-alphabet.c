7277f8863a8d49d50e2947b4dbef2b431d4f8a76
Massire <diabymassire93@gmail.com>
2026-03-03 09:26:27 +0000

    1-alphabet.c

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+*print_alphabet - prints the alphabet
+*
+*Return: void
+*/
+void print_alphabet(void)
+{
+charc;
+for (c = 'a'; c <= 'z'; c++)
+putchar(c);
+putchar('\n')
+}
