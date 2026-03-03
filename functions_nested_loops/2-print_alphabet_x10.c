156b8d387ed717d1a326d2d8e0c2798a60ac738c
Massire <diabymassire93@gmail.com>
2026-03-03 11:08:28 +0000

    2-print_alphabet_x10.c

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -6,21 +6,13 @@
  * Return: Always 0.
  */
 void print_alphabet_x10(void)
-
 {
 int i;
 char c;
 for (i = 0; i < 10; i++)
-
 {
 for (c = 'a'; c <= 'z'; c++)
-
 _putchar(c);
-
 _putchar('\n');
-
-return(0);
-
 }
-
 }
