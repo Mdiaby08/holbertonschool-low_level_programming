1e74774524551eb48d33bf55435494a2a4981541
Massire <diabymassire93@gmail.com>
2026-03-03 11:46:59 +0000

    3-islower.c

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* _islower - checks for lowercase character
+*
+*Returns: 1 lowercase
+*Returns: 0 otherwise 
+*/
+int _islower(int c);
+{
+int c;
+if (c >= 'a''z'<= c);
+return (1)
+return (0)
+}
