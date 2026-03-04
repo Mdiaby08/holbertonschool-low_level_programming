1a48eadd904416365f3cd30417afdf6335f5a7e6
Massire <diabymassire93@gmail.com>
2026-03-04 10:50:52 +0000

    5-sign.c

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+*print_sign - Print sign of a number
+*
+*Return: 1 greater than zero, 0 is zero, -1 less than zero
+*/
+
+int print_sign(int n);
+{
+if (n > 0)
+return (1);
+{
+if (n = 0)
+return (0);
+return (-1)
+}
+}
