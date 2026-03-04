348eb9cce6f23b7c64a96809285b7ac16cdaa907
Massire <diabymassire93@gmail.com>
2026-03-04 10:06:21 +0000

    4-isalpha.c

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* _islower - checks for lowercase character
+*
+*Returns: 1 lowercase, 0 otherwise
+*/
+
+int _isalpha(int c)
+{
+if (c >= 'a' && c <= 'z')
+return (1);
+if else (c >= 'A' && c <= 'Z')
+else
+return (0);
+}
