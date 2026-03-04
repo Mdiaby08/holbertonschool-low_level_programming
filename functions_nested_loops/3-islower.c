8add75aa1a000aceb9a5009ff66ce72dab628e72
Massire <diabymassire93@gmail.com>
2026-03-04 09:28:01 +0000

    3-islower.c code correction

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -3,22 +3,13 @@
 /**
 * _islower - checks for lowercase character
 *
-*Returns: 1 lowercase
-*Returns: 0 otherwise 
+*Returns: 1 lowercase, 0 otherwise 
 */
-int _islower(int c);
-int _putchar(char c);
-{
+
 int c;
 if (c >= 'a' && 'z'<= c);
 {
-_putchar (c)
 return (1)
-}
 else
-{
-_putchar (c)
-}
-
 return (0)
 }
