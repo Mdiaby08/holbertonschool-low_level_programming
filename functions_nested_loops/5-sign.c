6d8b97a4a603d4de46f55602f76f4715e16fe0f7
Massire <diabymassire93@gmail.com>
2026-03-04 11:16:23 +0000

    5-main.c code correction

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -5,12 +5,21 @@
 *
 *Return: 1 greater than zero, 0 is zero, -1 less than zero
 */
-
-int print_sign(int n);
+int print_sign(int n)
 {
 if (n > 0)
+{
+_putchar ('+');
 return (1);
-if (n = 0)
+}
+else if (n == 0)
+{
+_putchar ('0');
 return (0);
+}
+else 
+{
+_putchar ('-');
 return (-1);
+}
 }
