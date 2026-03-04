141799a578734648fe274a4bc25883778bbb3b36
Massire <diabymassire93@gmail.com>
2026-03-04 14:48:45 +0000

    10-add.c

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+*add.c - Add tow integrers
+*@a: first integrer
+*@b: second integrer
+*
+*Return: sum of a and b 
+*/
+int add(int a, int b)
+{
+return (a + b);
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -6,12 +6,12 @@ int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
+int add(int, int);
 int print_last_digit(int);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 void jack_bauer(void);
 void times_table(void);
-
-
+void print_to_98(int n);
 
 #endif
