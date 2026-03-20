bfecc2b05f24ff3eae7c7ad110f59325391e2d04
Massire <diabymassire93@gmail.com>
2026-03-20 20:27:58 +0000

    function ponter 0, 1 et 2

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+
+/**
+*print_name - prints a name using a function pointer
+*@name: name to print
+*@f: function pointer to a function that takes a char * and returns void
+*
+*Return: nothing
+*/
+void print_name(char *name, void (*f)(char *))
+{
+if (name == NULL || f == NULL)
+return;
+f(name);
+}
