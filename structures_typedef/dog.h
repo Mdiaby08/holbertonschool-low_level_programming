8c91cc4260364791c2fb6345a9f86b2273091c53
Massire <diabymassire93@gmail.com>
2026-03-20 19:35:14 +0000

    dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -14,9 +14,4 @@ struct dog
     char *owner;
 };
 
-/* Prototypes */
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
-void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif /* DOG_H */
