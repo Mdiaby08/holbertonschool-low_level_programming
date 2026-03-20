f43cc0b37c6aa9219efd664a9285ec421ed9741a
Massire <diabymassire93@gmail.com>
2026-03-20 09:49:18 +0000

    3-array_range.c correction

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -6,19 +6,24 @@
  * @min: minimum value
  * @max: maximum value
  *
- * Return: pointer to the array, or NULL on failure
+ * Return: pointer to the array or NULL
  */
 int *array_range(int min, int max)
 {
-int *a;
-int i, size;
-if (min > max)
-return (NULL);
-size = max - min + 1;
-a = malloc(size * sizeof(int));
-if (a == NULL)
-return (NULL);
-for (i = 0; i < size; i++)
-a[i] = min + i;
-return (a);
+    int *a;
+    int i, size;
+
+    if (min > max)
+        return (NULL);
+
+    size = max - min + 1;
+
+    a = malloc(size * sizeof(int));
+    if (a == NULL)
+        return (NULL);
+
+    for (i = 0; i < size; i++)
+        a[i] = min + i;
+
+    return (a);
 }
