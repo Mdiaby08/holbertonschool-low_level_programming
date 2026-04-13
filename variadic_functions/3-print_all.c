dbe71401038f4b67d04884e0c95323bcfc7c9e02
Mdiaby08 <diabymassire93@gmail.com>
2026-04-13 10:47:42 +0200

    change to betty code styles

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -10,44 +10,42 @@
  */
 void print_all(const char * const format, ...)
 {
-    va_list args;
-    int i = 0;
-    char *str;
-    char *sep = "";
+va_list args;
+int i = 0;
+char *str;
+char *sep = "";
 
-    va_start(args, format);
+va_start(args, format);
 
-    while (format && format[i])
-    {
-        if (format[i] == 'c' || format[i] == 'i' ||
-            format[i] == 'f' || format[i] == 's')
-        {
-            printf("%s", sep);
+while (format && format[i])
+{
+if (format[i] == 'c' || format[i] == 'i' ||
+format[i] == 'f' || format[i] == 's')
+{
+printf("%s", sep);
 
-            switch (format[i])
-            {
-                case 'c':
-                    printf("%c", va_arg(args, int));
-                    break;
-                case 'i':
-                    printf("%d", va_arg(args, int));
-                    break;
-                case 'f':
-                    printf("%f", (double)va_arg(args, double));
-                    break;
-                case 's':
-                    str = va_arg(args, char *);
-                    if (!str)
-                        str = "(nil)";
-                    printf("%s", str);
-                    break;
-            }
-
-            sep = ", ";
-        }
-        i++;
-    }
-
-    printf("\n");
-    va_end(args);
+switch (format[i])
+{
+case 'c':
+printf("%c", va_arg(args, int));
+break;
+case 'i':
+printf("%d", va_arg(args, int));
+break;
+case 'f':
+printf("%f", (double)va_arg(args, double));
+break;
+case 's':
+str = va_arg(args, char *);
+if (!str)
+str = "(nil)";
+printf("%s", str);
+break;
+}
+sep = ", ";
+}
+i++;
+}
+printf("\n");
+va_end(args);
 }
